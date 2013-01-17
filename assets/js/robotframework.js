7a0d3433ef34f597f1614854d4719a1a11fe4661
Pekka Klärck <peke@iki.fi>
2013-01-17 17:28:16 +0200

    enabled tooltips etc.
    Signed-off-by: Pekka Klärck <peke@iki.fi>

--- assets/js/robotframework.js
+++ assets/js/robotframework.js
@@ -1,20 +1,25 @@
 function init_tweets() {
-  $("#tweets").tweet({
-      "username": "robotframework"
-    , "count": 10
-    , "template": "{text} {time}"
-  });  
-
+    $("#tweets").tweet({
+        "username": "robotframework",
+        "count": 10,
+        "template": "{text} {time}"
+    });
 }
 
 function init_carousel() {
-  $( "#example-carousel" ).carousel({
-    "interval": 8000
-  });
-  
+    $("#example-carousel").carousel({
+        "interval": 8000
+    });
 }
 
-$( document ).ready(function() {
-  init_tweets();
-  init_carousel();
+function init_tooltips() {
+    $(".sub-section a").tooltip({
+        "placement": "left"
+    });
+}
+
+$(document).ready(function() {
+    init_tweets();
+    init_carousel();
+    init_tooltips();
 });
