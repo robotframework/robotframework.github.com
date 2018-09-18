2a25f8d74ee70909f5de70c7ef72e08caf4eb76a
Antti Karjalainen <antti.karjalainen@siili.com>
2018-09-18 19:17:33 +0200

    Revert recent changes

--- sources/src/App.vue
+++ sources/src/App.vue
@@ -1205,13 +1205,6 @@ export default {
                   'Kistler is using the Robot Framework for automated system tests of backend and frontend applications for various, very accurate and highly precise <a href="https://www.kistler.com/en/products/systems/" target="_blank">measurement systems</a>.'
               },
               {
-                title: "Rocla",
-                href: "http://rocla.com/",
-                img: require("@/assets/img/users/rocla.png"),
-                text:
-                  "Rocla is using Robot Framework widely in development and testing of the software for trucks and Automated Guided Vehicles (AGV’s)."
-              },
-              {
                 title: "salicru",
                 href: "http://www.salicru.com",
                 img: require("@/assets/img/users/salicru.png"),
