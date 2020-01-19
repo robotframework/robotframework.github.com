836f9481a88e0345383e3865a0629c20b52ddf09
Sami Kolari <sami.kolari@siilim180518.local>
2020-01-19 19:07:50 +0200

    remove robocon banner

--- sources/src/components/AppHeader.vue
+++ sources/src/components/AppHeader.vue
@@ -5,21 +5,6 @@
           <h1 class="main-header">ROBOT FRAME WORK/</h1>
         </div>
       </section>
-      <div class="robocon-banner row">
-        <div class="col-lg-5 col-xl-4">
-          <a href="https://robocon.io">
-          <h1>LET'S</h1>
-          <h1>TALK</h1>
-          <br>
-          <h2 class="mb-1" style="color: white;">ROBOCON</h2>
-          <h2>2020</h2>
-          </a>
-        </div>
-        <div class="col-lg-7 col-xl-8 pt-5 mb-5">
-          Early tickets are on sale. Get yours <a href="https://robocon.io/#tickets">here!</a><br><br>
-          If you’re looking to get deeper into specific topics, check out our workshops. There’s limited availability, so make sure <a href="https://robocon.io/#workshops">to book early.</a>
-        </div>
-      </div>
     </div>
 </template>
 
