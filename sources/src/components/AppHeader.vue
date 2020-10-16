59994ead241ca5b9d2ab8421740ab9ddbf776e19
Sami Kolari <sami.kolari@siilim180518.local>
2020-10-16 17:48:01 +0300

    WWWW banner

--- sources/src/components/AppHeader.vue
+++ sources/src/components/AppHeader.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
+      <section class="header wwww">
+        <a href="https://robot-work.shop/">
+          <h2 class="main-header wwww">World <span style="color: black;">Wide</span> Workshop <span style="color: black;">Wednesday </span></h2>
+          <h3 class="wwww">02/<span style="color: black;">DEC</span>/2020</h3>
+        </a>
+      </section>
       <section class="header">
         <div class="main-header-container">
           <h1 class="main-header">ROBOT FRAME WORK/</h1>
