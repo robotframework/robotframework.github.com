b39b9b0bfbba93cb67e538f4236277ea8f7ab328
Sami Kolari <sami_kolari@hotmail.com>
2022-04-04 23:22:50 +0300

    remove fundraiser

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,29 +1,10 @@
 <template>
-  <div>
-    <div class="row center p-medium pb-xsmall ukraine-blue">
-      <div class="col-sm-12 type-center mb-xsmall weight-bold">
-        <a
-          href="https://oma.unicef.fi/en_US/participant/robot-framework-ry-virtual-workshop-to-help-children-in-ukraine"
-          target="_blank"
-          class="color-white">
-          <div>
-            Unicef fundraiser:
-          </div>
-          <div>
-            Help children in Ukraine
-          </div>
-        </a>
-      </div>
-       <div class="type-center mt-small weight-bold">
-      </div>
-      <h1 class="title type-center">
-        ROBOT
-      </h1>
-    </div>
-    <div class="row center p-medium pt-none ukraine-yellow">
-      <h1 class="type-center">
+  <div class="row bg-theme center p-medium">
+    <div>
+      <h1 class="title">
+        ROBOT<br>
         FRAME<br>
-        WORK/
+        WORK<div style="font-size: 2.5rem; transform: translateY(-.5rem); display: inline-block;">🇺🇦</div>
       </h1>
     </div>
   </div>
@@ -39,18 +20,6 @@ export default {
 
   .title::first-line {
     color: var(--color-white);
-  }
-
-  .ukraine-blue {
-    background: rgb(37,90,181);
-  }
-
-  .ukraine-yellow {
-    background: rgb(248,215,72);
-  }
-
-  .ukraine {
-    background: linear-gradient(180deg, rgba(37,90,181,1) 50%, rgba(248,215,72,1) 50%);
   }
 
 </style>
