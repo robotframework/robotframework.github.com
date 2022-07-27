a7ec4bf2eabe6e4143c1c664dc23e3be785c2ff2
Sami Kolari <sami_kolari@hotmail.com>
2022-07-27 23:46:11 +0300

    foundation blog post banner

--- src/content/english/main.js
+++ src/content/english/main.js
@@ -5,7 +5,7 @@ export default () => ({
   ...foundation(),
   ...rpa(),
   langName: 'English',
-  newsBanner: 'Introducing Robot Framework blog! You can read our thoughts of RoboCon 2022 <a href="https://medium.com/{at}{\'@\'}{at}RobotFramework/robocon-2022-epilogue-f1160a89b1ca">in the blogs first post</a>.',
+  newsBanner: '<a href="https://medium.com/{at}{\'@\'}{at}RobotFramework/robot-framework-foundation-a37a8bcec74f">New blog post: Robot Framework Foundation</a>',
   navbar: {
     items: [
       {
