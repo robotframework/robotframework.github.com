43b646fd6511fbe941ceb987a1462993ac737455
Mikko Korpela <mikko.korpela@gmail.com>
2022-08-07 20:37:13 +0300

    fix videos

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -4,7 +4,7 @@
       :style="{ 'overflow-x': 'scroll', 'overflow-y' : 'hidden', 'margin-top': '25px !important' }"
   >
     <iframe style="position: relative; width: 640px; height: 390px;  border: 1px solid black; "
-            :src="'http://www.youtube.com/embed/' + videoId + '?html5=1&amp;enablejsapi=1&amp;autoplay=0&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;controls=1&amp;autohide=0&amp;vq=large'" frameborder="0" allowfullscreen
+            :src="'https://www.youtube.com/embed/' + videoId + '?html5=1&amp;enablejsapi=1&amp;autoplay=0&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;controls=1&amp;autohide=0&amp;vq=large'" frameborder="0" allowfullscreen
             onload="player1=new YT.Player(this)">
     </iframe>
   </div>
