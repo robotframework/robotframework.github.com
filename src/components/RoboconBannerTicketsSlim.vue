2b04a8c11d1cfd71ec7e50876e9d8effbdba423a
samipe <sami_kolari@hotmail.com>
2023-12-12 07:28:04 +0200

    rbcn banner style fix + build

--- src/components/RoboconBannerTicketsSlim.vue
+++ src/components/RoboconBannerTicketsSlim.vue
@@ -1,9 +1,9 @@
 <template>
-      <div class="font-title bg-black color-white type-left type-slimbanner">
-        <div class="mt-small" style="float: left">
+      <div class="font-title bg-black color-white type-left type-slimbanner between flex type-large">
+        <div class="m-small">
           Tickets for <span class="type-uppercase color-robocon">Robocon 2024</span> are on sale now!
         </div>
-        <div class="mt-small" style="float: right">
+        <div class="m-small">
           More info at <a class="color-robocon" href="https://robocon.io/">robocon.io</a>.
         </div>
       </div>
@@ -14,7 +14,16 @@
   color: #b861ff;
 }
 
-.type-slimbanner {
-  font-size: var(--type-large);
+@media screen and (max-width: 1000px) {
+  .type-slimbanner {
+    justify-content: start;
+    flex-wrap: wrap;
+    font-size: var(--font-body);
+
+    & > div {
+      margin: 0.5rem;
+      margin-left: 1rem;
+    }
+  }
 }
 </style>
