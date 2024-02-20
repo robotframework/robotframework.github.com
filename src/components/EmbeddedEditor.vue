0f651c365e3b9c4095db12b217294786d86ea060
René <snooz@posteo.de>
2024-02-20 20:24:24 +0100

    fixed design, copy and requirements execution
    Signed-off-by: René <snooz@posteo.de>

--- src/components/EmbeddedEditor.vue
+++ src/components/EmbeddedEditor.vue
@@ -51,6 +51,12 @@
           <!-- run buttons -->
           <div v-if="!editorStatus.loading" class="flex">
             <button
+              class="stroke mr-xsmall small flex middle"
+              aria-label="Share code"
+              @click="copyProject()">
+              <copy-icon size="1rem" color="white" />
+            </button>
+            <button
               class="theme flex middle"
               :class="editorStatus.running ? 'disabled' : 'bling'"
               @click="runRobotTest()">
@@ -101,7 +107,7 @@
         </pre>
       </div>
       <!-- modal buttons -->
-      <div class="button-bar border-top-theme border-thin col-sm-12 col-md-5 flex height-fit p-xsmall">
+      <div class="button-bar border-top-theme border-thin col-sm-12 flex height-fit p-xsmall">
         <transition name="opacity">
           <div v-if="logSrc">
             <button class="bg-grey-darkest stroke small flex mr-small middle" @click="showLog = true">
@@ -150,6 +156,7 @@ import * as LZString from 'Code/lz-string'
 import ChevronIcon from './icons/ChevronIcon.vue'
 import PlayIcon from './icons/PlayIcon.vue'
 import DocumentIcon from './icons/DocumentIcon.vue'
+import CopyIcon from './icons/CopyIcon.vue'
 import CloseIcon from './icons/CloseIcon.vue'
 let editor = {}
 let codeLens = null
@@ -172,6 +179,7 @@ export default {
     ChevronIcon,
     PlayIcon,
     DocumentIcon,
+    CopyIcon,
     CloseIcon
   },
   data: () => ({
@@ -216,13 +224,55 @@ export default {
     clickFn(ev) {
       if (this.$refs.fileDropdown && !this.$refs.fileDropdown.contains(ev.target)) this.filesDropdownOpen = false
     },
+    async copyProject() {
+      const compProj = await this.getProjectLink()
+      const url = document.location.origin + '/embed/?codeProject=' + compProj
+      console.log(url.length)
+      if (url.length > 7400) {
+        this.copyMessage = { message: `Code to be shared is too long! ~${url.length - 7400} too many characters...`, success: false }
+      } else {
+        await navigator.clipboard.writeText(url)
+        this.copyMessage = { message: 'Link copied to clipboard!', success: true }
+      }
+    },
+    async getProjectLink() {
+      const isOfficialProject = this.projectsList.some(({ name }) => name === this.activeProjectName)
+      const files = Object.entries(models).map((model) => {
+        const content = model[1].getValue()
+        const fileFromProject = this.activeProject.files.find(({ fileName }) => fileName === model[0])
+
+        if (fileFromProject.content !== content || !isOfficialProject) {
+          return {
+            fileName: model[0],
+            content: model[1].getValue()
+          }
+        }
+        return null
+      })
+        .filter((a) => a)
+      const project = {
+        name: this.activeProjectName,
+        description: '',
+        files: files.filter(({ hidden }) => !hidden),
+        derivedProject: isOfficialProject,
+        robotVersion: this.selectedRFVersion,
+        robotArgs: this.robotArgs,
+        requirements: this.requirements
+      }
+      console.log(project)
+      const strProj = JSON.stringify(project)
+      const compProj = LZString.compressToEncodedURIComponent(strProj)
+      console.log(`Size of compressed Base 64 fileCatalog is: ${compProj.length} (${compProj.length / (strProj.length / 100)}%)`)
+      return compProj
+    },
     async setProjectFromGitHub(ghURL) {
-      var project = await getProjectFromGitHub(ghURL)
+      const project = await getProjectFromGitHub(ghURL)
       this.setProject(project, 'Custom code')
     },
     async setProjectsFromURL(codeProject) {
       const strProj = LZString.decompressFromEncodedURIComponent(codeProject)
-      var project = JSON.parse(strProj)
+      const project = JSON.parse(strProj)
+      console.log(project)
       if (project.derivedProject) {
         const proj = await getProjectFromLiveDir(this.projectsList.find(({ name }) => name === project.name).dir)
         proj.files = proj.files
@@ -232,6 +282,7 @@ export default {
             content: project.files.find(({ fileName }) => fileName === file.fileName)?.content || file.content
           }))
         proj.robotVersion = project.robotVersion
+        console.log(proj)
         this.setProject(proj, proj.name)
       } else {
         this.setProject(project, 'Custom code')
@@ -357,7 +408,7 @@ export default {
     const theme = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'rf-dark' : 'rf-light'
     editor = monaco.editor.create(document.getElementById('monaco-container'), {
       language: 'robotframework',
-      theme: theme,
+      theme,
       wordWrap: 'off',
       automaticLayout: true,
       minimap: {
@@ -389,8 +440,8 @@ export default {
       contextMenuOrder: 0,
       run: (ed) => { this.runRobotTest() }
     })
-    var commandRunSuite = editor.addCommand(0, (ctx, tcName) => { this.runRobotTest(false, tcName) }, '')
-    var commandResetSuite = editor.addCommand(0, (ctx, tcName) => { this.resetProject() }, '')
+    const commandRunSuite = editor.addCommand(0, (ctx, tcName) => { this.runRobotTest(false, tcName) }, '')
+    const commandResetSuite = editor.addCommand(0, (ctx, tcName) => { this.resetProject() }, '')
 
     codeLens = monaco.languages.registerCodeLensProvider('robotframework', {
       provideCodeLenses: function(model, token) {
@@ -411,7 +462,7 @@ export default {
           }
         }
         const testCases = getTestCaseRanges(model)
-        var lenses = testCases.map((testCase) => {
+        const lenses = testCases.map((testCase) => {
           return getTestCaseLense(testCase)
         })
         lenses.push({
@@ -443,7 +494,7 @@ export default {
           }
         })
         return {
-          lenses: lenses,
+          lenses,
           dispose: () => {}
         }
       },
--- src/js/code/index.js
+++ src/js/code/index.js
@@ -17,7 +17,7 @@ const getProjectFromGitHub = async(ghURL) => {
   console.log(`Fetching example from GitHub project: ${url}`)
   const downloadURL = `https://raw.githubusercontent.com/${user}/${repo}/${branch || 'main'}${(path) ? '/' + path : ''}`
   console.log(downloadURL)
-  var project = await getProject(downloadURL)
+  const project = await getProject(downloadURL)
   // project.description = `## ⚠️ Caution: User Created Content\n\nBe aware that this code is created by a user of that page and not by Robot Framework Foundation. Therefore we are not liable for the content. The code is loaded from the following GitHub Repo.\n<a href="${url}" target="_blank">${url}</a>\n\nIf you run this code it will be executed in your browser.\n\n---\n${project.description}`
   return project
 }
@@ -31,7 +31,7 @@ const getProject = async(projectUrl) => {
   console.log(`Loading data from ${projectUrl}`)
   const configFile = await fetch(projectUrl + '/config.json')
     .then(response => response.json())
-  var project = { name: configFile.name, files: [], description: '' }
+  const project = { name: configFile.name, files: [], description: '' }
   if (configFile.robotVersion) {
     project.robotVersion = configFile.robotVersion
   }
@@ -48,7 +48,7 @@ const getProject = async(projectUrl) => {
   }
   for (const file of configFile.files) {
     const { fileName, hidden } = file
-    const content = await fetch(projectUrl + '/' + fileName)
+    const content = await fetch(`${projectUrl}/${fileName}?token=${Date.now()}`)
       .then(response => response.text())
     project.files.push({
       fileName,
