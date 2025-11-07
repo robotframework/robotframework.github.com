c6828a602f12654d5998c364814cc2319307e946
Markus Stahl <markus.stahl@imbus.de>
2025-11-07 15:33:17 +0100

    Update content to 2025

--- src/content/english/rpa.js
+++ src/content/english/rpa.js
@@ -44,8 +44,7 @@ export default () => ({
       <p>Robot Framework is a keyword-driven automation framework that sits on top of Python. It is an
       abstraction that lets you write powerful automations with simple human-readable commands and minimum overhead.</p>
 
-      <p>The power of Robot Framework comes from its ecosystem of tools, libraries, and community. It is
-      the world’s most popular and widely used framework for open-source RPA.</p>`
+      <p>The power of Robot Framework comes from its ecosystem of tools, libraries, and community.</p>`
     },
     benefits: {
       title: 'Why use it and what can you do with it?',
@@ -64,49 +63,47 @@ export default () => ({
       onboarding. You can complete any routine task that a human would do through digital workers built on the Robot Framework.</p>`
     },
     quickstart: {
-      title: 'Quickstart',
-      body: `<p>Quickstart is still work in progress and will be released later this year.
-      Meanwhile, check User Guide for general <a href="https://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#creating-tasks">task creation instructions</a>!</p>`
+      title: 'General User Guide',
+      body: '<p>Check User Guide for general <a href="https://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#creating-tasks">task creation instructions</a>!</p>'
     },
     resources: {
       title: 'Resources',
-      body: `<p>Robot Framework itself provides the language to describe automations. To create an RPA
-      bot with Robot Framework, you’ll combine tooling (develop, package, run) with libraries (bot capabilities.)
-      This list is a collection of RPA resources to get you started</p>`,
+      body: `<p>Robot Framework itself provides the language to describe automations. Task automation is what is covered by Robot Framework. 
+      For orchestrating tasks you need a some sort of orchestrator.</p>`,
       tooling: {
         title: 'Tooling',
         description: 'Tools for developing and running bots with Robot Framework',
         list: [
           {
-            name: 'RCC',
-            href: 'https://github.com/robocorp/rcc#readme',
-            description: 'Toolchain for creating, packaging, and running bots with Robot Framework and Python'
+            name: 'Operaton',
+            href: 'https://operaton.org',
+            description: 'BPMN based workflow engine orchestrating tasks'
           }, {
-            name: 'Robot Framework Language Server for VS Code',
-            href: 'https://marketplace.visualstudio.com/items?itemName=robocorp.robotframework-lsp',
-            description: 'VS Code extension for Robot Framework language'
+            name: 'Purjo',
+            href: 'https://github.com/datakurre/purjo',
+            description: 'UV based Robot Framework external service task runner for Operaton'
           }, {
-            name: 'RCC features for VS Code',
-            href: 'https://marketplace.visualstudio.com/items?itemName=robocorp.robocorp-code',
-            description: 'VS Code extension for RCC'
+            name: 'Robotcode',
+            href: 'https://robotcode.dev',
+            description: 'Robot Framework extension for VS Code and PyCharm'
           }
         ]
       },
       libraries: {
-        title: 'Libraries',
-        description: 'RPA focused / useful libraries',
+        title: 'Robot Framework Extensions',
+        description: 'Useful libraries',
         list: [
           {
-            name: 'RPA framework',
-            href: 'https://rpaframework.org',
-            description: 'Common RPA functionality in a single library - includes desktop, browser, Excel, PDF, email, and many more capabilities'
+            name: 'MarketSquare',
+            href: 'https://github.com/MarketSquare',
+            description: 'Collection of libraries for Robot Framework'
           }, {
-            name: 'Playwright',
+            name: 'Browser',
             href: 'https://robotframework-browser.org',
             description: 'Powerful browser automation library utilizing Playwright. It comes with a built-in browser.'
           }, {
-            name: 'SAP GUI Library',
-            href: 'https://github.com/frankvanderkuur/robotframework-sapguilibrary',
+            name: 'RoboSAPiens',
+            href: 'https://github.com/imbus/robotframework-robosapiens',
             description: 'GUI automation for SAP'
           }, {
             name: 'Mainframe 3270 Library',
@@ -116,6 +113,11 @@ export default () => ({
             name: 'DataDriver Libary',
             href: 'https://github.com/Snooz82/robotframework-datadriver',
             description: 'Data-driven automation through tables (csv, xls, xlsx, etc.)'
+          },
+          {
+            name: 'Pabot',
+            href: 'https://pabot.org',
+            description: 'Parallel execution of Robot Framework tasks'
           }
         ]
       },
@@ -124,13 +126,14 @@ export default () => ({
         description: '',
         list: [
           {
-            name: 'Robocorp Portal',
-            href: 'https://robocorp.com/portal/',
-            description: 'Examples and templates for RPA projects'
-          }, {
-            name: 'Robocorp RPA certifications',
-            href: 'https://robocorp.com/docs/courses',
-            description: 'Robocorp certification trainings for RPA (free)'
+            name: 'Operaton Playground',
+            href: 'https://datakurre.github.io/operaton-robot-playground/playground/index.html',
+            description: 'Full featured training for Process automation with Operaton and Robot Framework.'
+          },
+          {
+            name: 'Robotbook with Robotcode',
+            href: 'https://forum.robotframework.org/t/missing-super-useful-functionalities-live-mode-record-execute-from-spreadsheet/8994/10',
+            description: 'Example for REPL based task automation.'
           }
         ]
       }
